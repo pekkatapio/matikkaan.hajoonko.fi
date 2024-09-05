949ca9c75a74afa201ddc139682d538344be0b0c
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2024-09-05 07:58:29 +0300

    uudelleenjärjestää ja dokumentoi JavaScript-funktiot

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-/* Randomize array in-place using Durstenfeld shuffle algorithm */
+/* 
+  Apufunktiot 
+ 
+  shuffleArray
+    Randomize array in-place using Durstenfeld shuffle algorithm 
+*/
 function shuffleArray(array) {
   for (var i = array.length - 1; i >= 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -8,11 +13,18 @@ function shuffleArray(array) {
   }
 }
 
-function fillContent(tasks) {
+/* 
+  Dokumentin muokkausfunktiot
+
+  fillContentWithTasks
+    Lisää tasks-taulukossa olevat tehtävät dokumentin 
+    tasks-tunnisteella olevaan elementtiin.
+*/    
+function fillContentWithTasks(tasks, groups=4, rows=25) {
   let content = "";
-  for (let group = 1; group <= 4; group++) {
+  for (let group = 1; group <= groups; group++) {
     content = content + "<div class='taskgroup'>"
-    for (let row = 1; row <= 25; row++) {
+    for (let row = 1; row <= rows; row++) {
       let task = tasks.shift();
       content = content + "<div>" + task + "</div>";
     }
@@ -22,20 +34,12 @@ function fillContent(tasks) {
   document.getElementById("tasks").innerHTML = content;
 }
 
-function generateSingleMultiplicationTable(multiplier) {
-  let multiplicands = [1,2,2,3,3,4,4,4,4,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10];
-  let tasks = [];
-  for (let round = 1; round <= 4; round++) {
-    let base = multiplicands.slice();
-    shuffleArray(base);
-    for (let index = 0; index < base.length; index++) {
-      tasks.push(multiplier + " &times; " + base[index] + " = ______");
-    }
-  }
-  fillContent(tasks);
-  fillVisitors("matikkaan.hajoonko.fi/kertotaulu");
-}
-
+/*
+  fillVisitors
+    Selvittää käyntikertojen määrän kutsumalla API-kutsua 
+    ja lisää inforivin visits-tunnisteella olevaan 
+    elementtiin.
+*/   
 function fillVisitors(site) {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", "https://neutroni.hayo.fi/~pta/api/count/" + site);
@@ -46,6 +50,13 @@ function fillVisitors(site) {
   xhr.send();  
 }
 
+/*
+  Tehtävien luontifunktiot
+
+  generateMultiplicationTable
+    Muodostaa lukujen 1-10 kertotaulun (100 tehtävää) ja 
+    täytää sivun niillä.
+*/
 function generateMultiplicationTable() {
   let tasks = [];
   for (let first = 1; first <= 10; first++ ) {
@@ -54,6 +65,25 @@ function generateMultiplicationTable() {
     }    
   }
   shuffleArray(tasks);
-  fillContent(tasks);
+  fillContentWithTasks(tasks);
+  fillVisitors("matikkaan.hajoonko.fi/kertotaulu");
+}
+
+/*
+  generateMultiplicationTable
+    Muodostaa annetun luvun kertotaulun (28 x 4 = 112 tehtävää)
+    ja lisää sivulle niistä satunnaisesti 100 tehtävää.
+*/
+function generateSingleMultiplicationTable(multiplier) {
+  let multiplicands = [1,2,2,3,3,4,4,4,4,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10];
+  let tasks = [];
+  for (let round = 1; round <= 4; round++) {
+    let base = multiplicands.slice();
+    shuffleArray(base);
+    for (let index = 0; index < base.length; index++) {
+      tasks.push(multiplier + " &times; " + base[index] + " = ______");
+    }
+  }
+  fillContentWithTasks(tasks);
   fillVisitors("matikkaan.hajoonko.fi/kertotaulu");
 }
